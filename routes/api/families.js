a58f1f7c2ea2ff96fa660eb42ad80d7aa409ea19
pedramamiri <32764741+pedramamiri@users.noreply.github.com>
2018-12-10 21:04:43 +0100

    nodemailer

--- routes/api/families.js
+++ routes/api/families.js
@@ -1,8 +1,65 @@
 const express = require('express');
 const router  = express.Router();
+var nodemailer = require('nodemailer');
+
+
+
+function sendEmail(){
+    
+    return new Promise((resolve,reject)=>{
+       let transporter = nodemailer.createTransport({
+           host: 'smtp.ethereal.email',
+           port: 587,
+           auth: {
+               user: 'oevq6lqoj25xdwzb@ethereal.email',
+               pass: 'nAGHMggf5P3ZG1Vqk4'
+           },
+           tls:{
+               rejectUnauthorized:false
+           }
+       });
+       
+       let message = {
+           from: '<oevq6lqoj25xdwzb@ethereal.email>',
+           to: '<pedramrad1368@gmail.com>',
+           subject: 'Nodemailer is unicode friendly ✔',
+           text: 'Hello to myself!',
+           html: '<p><b>Hello</b> to myself!</p>'
+       };
+       
+       transporter.sendMail(message, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+          resolve('sent')
+        }
+      });
+   
+   }) 
+}
+
+router.get('/sendmail',(req,res)=>{
+    sendEmail()
+        .then((msg)=>res.send(msg))
+        .catch(err=>console.log(err))
+    
+});
+
+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
+
+
+
+
+    
+        
+  
+
+
 
 //user model 
 const Family = require('../../models/Family');
+
 
 // @route    GET api/families
 // @desc     Get all families
@@ -17,7 +74,7 @@ router.get('/',(req,res)=>{
 // @desc     Add a new family
 // @access   Public
 router.post('/',(req,res)=>{
-     
+
     const newFamily = new Family({
         familyName  : req.body.familyName,
         familyCode  : req.body.familyCode,
@@ -31,9 +88,11 @@ router.post('/',(req,res)=>{
         ))    
     })
     newFamily.save()
-        .then((family)=> res.json(family))
+        .then(family=>res.json(family))
         .catch((err)=> console.log(err))
 });
+
+
 
 // @route    DELETE api/families
 // @desc     Delete a family
